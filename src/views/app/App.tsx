96d35ef69d7c7a82978f70a194af3c3757109b7f
pdelpriore <paul.del.priore29@gmail.com>
2021-01-18 10:52:10 +0100

    add capitalize methods + logo

--- src/views/app/App.tsx
+++ src/views/app/App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 import "./app.css";
 
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      <Row className="mt-4" />
+      <Row>
+        <Col xs={1} />
+        <Col xs={11}>
+          <div className="logo">
+            <span className="logo-content">generujcv</span>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default App;
