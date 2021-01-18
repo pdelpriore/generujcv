da982a1841f7cdb2650007e1ced7ce76077936f3
pdelpriore <paul.del.priore29@gmail.com>
2021-01-18 11:36:27 +0100

    add responsive design

--- src/views/app/App.tsx
+++ src/views/app/App.tsx
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import "./app.css";
 
 const App: React.FC = () => {
+  const [viewport, setViewport] = useState<number>(0);
+
+  useEffect(() => {
+    setViewport(window.innerWidth);
+  }, []);
+
   return (
     <div className="container">
       <Row className="mt-4" />
       <Row>
-        <Col xs={1} />
-        <Col xs={11}>
+        <Col xl={1} xs={2} />
+        <Col xl={11} xs={10}>
           <div className="logo">
             <span className="logo-content">generujcv</span>
           </div>
         </Col>
+      </Row>
+      {viewport > 319 && viewport <= 374 ? (
+        <Row className="mt-3" />
+      ) : (
+        <Row className="mt-5" />
+      )}
+      <Row>
+        <Col xs={1} />
+        <Col xs={10}>
+          <div className="main"></div>
+        </Col>
+        <Col xs={1} />
       </Row>
     </div>
   );
