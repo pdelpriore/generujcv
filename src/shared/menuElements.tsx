58d1ed8736bbd894d99c2569c80cc8e5b3441d31
pdelpriore <paul.del.priore29@gmail.com>
2021-01-27 10:46:18 +0100

    clean code

--- src/shared/menuElements.tsx
+++ src/shared/menuElements.tsx
@@ -23,7 +23,7 @@ export const menuElements: MenuElements[] = [
   { icon: faFileAlt, content: "dane kontaktowe" },
   { icon: faLanguage, content: "języki obce" },
   { icon: faFistRaised, content: "mocne strony" },
-  { icon: faHiking, content: "hobby" },
+  { icon: faHiking, content: "zainteresowania" },
   { icon: faUniversity, content: "dyplomy" },
   { icon: faIndustry, content: "doświadczenie" },
   { icon: faFileWord, content: "kompetencje" },
