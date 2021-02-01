b56a1c9bcadb47afa49528526070e057c55b0dc2
pdelpriore <paul.del.priore29@gmail.com>
2021-02-01 16:16:44 +0100

    add language level value

--- src/shared/menuElements.tsx
+++ src/shared/menuElements.tsx
@@ -17,6 +17,10 @@ interface MenuElements {
   content: string;
 }
 
+interface LanguageLevelValue {
+  [key: string]: number;
+}
+
 export const menuElements: MenuElements[] = [
   { icon: faPortrait, content: "zdjęcie" },
   { icon: faAddressCard, content: "dane osobowe" },
@@ -46,3 +50,12 @@ export const maritalStatus: string[] = [
 export const disabilityLevel: string[] = ["lekka", "umiarkowana", "znaczna"];
 
 export const languagelevels: string[] = ["A1", "A2", "B1", "B2", "C1", "C2"];
+
+export const languageLevelValue: LanguageLevelValue = {
+  A1: 1,
+  A2: 2,
+  B1: 3,
+  B2: 4,
+  C1: 5,
+  C2: 6,
+};
