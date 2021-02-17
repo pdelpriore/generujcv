5ab1317022944b3b5aec109667119204942b6501
pdelpriore <paul.del.priore29@gmail.com>
2021-02-17 11:33:27 +0100

    add project form buttons

--- src/components/form/project/ProjectForm.tsx
+++ src/components/form/project/ProjectForm.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useContext } from "react";
 import { ProjectFormContext } from "../../../context/projectForm/ProjectFormContext";
+import TButton from "../../button/TButton";
 import { Row, Col, Form } from "react-bootstrap";
 import { capitalize } from "../../../methods/capitalize";
 import "./projectForm.css";
@@ -17,6 +18,12 @@ const ProjectForm: React.FC = () => {
     clearForm,
   } = useContext(ProjectFormContext);
 
+  useEffect(() => {
+    return () => {
+      if (isProjectEditing) clearForm();
+    };
+  }, [isProjectEditing, clearForm]);
+
   return (
     <Form className="formProject">
       <Row>
@@ -28,6 +35,10 @@ const ProjectForm: React.FC = () => {
             </Form.Label>
             <Form.Control
               className="formProject__control"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                onChangeGithub(e)
+              }
+              value={inputs.githubUrl || ""}
               name="githubUrl"
               type="text"
               placeholder="https://github.com/jankowalski"
@@ -43,6 +54,10 @@ const ProjectForm: React.FC = () => {
           </Form.Label>
           <Form.Control
             className="formProject__control"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              onChangeProject(e)
+            }
+            value={inputs.project.name || ""}
             name="name"
             type="text"
             placeholder="nazwa projektu"
@@ -54,9 +69,13 @@ const ProjectForm: React.FC = () => {
           </Form.Label>
           <Form.Control
             className="formProject__control"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              onChangeProject(e)
+            }
+            value={inputs.project.url || ""}
             name="url"
             type="text"
-            placeholder="link do projektu"
+            placeholder="https://awesomeapp.herokuapp.com"
           />
         </Form.Group>
         <Form.Group controlId="formProjectDescription">
@@ -67,11 +86,45 @@ const ProjectForm: React.FC = () => {
             className="formProject__control"
             as="textarea"
             rows={4}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              onChangeProject(e)
+            }
+            value={inputs.project.description || ""}
             name="description"
             type="text"
             placeholder="opis projektu"
           />
         </Form.Group>
+        {isProjectEditing ? (
+          <div className="formProject__btn-wrapper">
+            <TButton
+              className="formProject__btn"
+              type="add"
+              content="popraw"
+              onClick={editProject}
+            />
+            <TButton
+              className="formProject__btn"
+              type="cancel"
+              content="anuluj"
+              onClick={cancelEditProject}
+            />
+          </div>
+        ) : (
+          <div className="formProject__btn-wrapper">
+            <TButton
+              className="formProject__btn"
+              disabled={
+                inputs.project.name.length === 0 ||
+                inputs.project.description.length === 0 ||
+                inputList.projects.length === 5
+              }
+              type="add"
+              content="dodaj"
+              onClick={addProject}
+            />
+          </div>
+        )}
       </div>
     </Form>
   );
