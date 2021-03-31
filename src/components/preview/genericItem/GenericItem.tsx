a264bc2ea99bd4855dbc6b1d44c3bb65ef34e9e4
pdelpriore <paul.del.priore29@gmail.com>
2021-03-31 13:59:10 +0200

    clean code

--- src/components/preview/genericItem/GenericItem.tsx
+++ src/components/preview/genericItem/GenericItem.tsx
@@ -8,7 +8,9 @@ interface GenericItemProps {
 const GenericItem: React.FC<GenericItemProps> = ({ content }) => {
   return (
     <div className="generic">
-      <div className="generic__icon"></div>
+      <div className="generic__icon">
+        <div className="generic__icon-ico"></div>
+      </div>
       <span className="generic__content">{content}</span>
     </div>
   );
